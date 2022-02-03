c3c136556d47470e113e459b6d6e4058bd418cd7
Ritik-Arora-576 <ritikarora656@gmail.com>
2022-02-04 00:53:31 +0530

    finalize

--- main.py
+++ main.py
@@ -12,9 +12,11 @@ def login():
         return redirect('/home')
     return render_template("login.html")
 
+
 @app.route('/create')
 def create():
     return render_template("SignUp.html")
+
 
 @app.route('/check_login', methods=['POST'])
 def check_login():
@@ -22,10 +24,11 @@ def check_login():
         dict = pkl.load(f)
     email = request.form['email']
     password = request.form['password']
-    if (email in dict) and (dict[email][1]==password):
-        session['name']=dict[email][0]
+    if (email in dict) and (dict[email][1] == password):
+        session['name'] = dict[email][0]
         return redirect('/home')
     return redirect('/')
+
 
 @app.route('/register', methods=['POST'])
 def register():
@@ -43,16 +46,19 @@ def register():
         return redirect('/')
     return redirect('/create')
 
+
 @app.route('/logout')
 def logout():
     session.pop('name')
     return redirect('/')
 
+
 @app.route("/home")
 def hello():
     if 'name' in session:
-        return render_template("home.html", name =session['name'])
+        return render_template("home.html", name=session['name'])
     return redirect('/')
+
 
 @app.route("/results", methods=["POST", "GET"])
 def results():
@@ -61,33 +67,37 @@ def results():
             movie = request.form['movie_name']
             movie = top_reccomended_movies(movie)
 
-        return render_template("result.html", movies = movie, name =session['name'])
+        return render_template("result.html", movies=movie, name=session['name'])
     return redirect('/')
+
 
 @app.route('/<int:id>')
 def details(id):
     if 'name' in session:
         with open('results/details.pkl', 'rb') as f:
             dict = pkl.load(f)
-        return render_template("details.html", data = dict[id], name =session['name'])
+        return render_template("details.html", data=dict[id], name=session['name'])
     return redirect('/')
+
 
 @app.route('/<string:genre>')
 def genres(genre):
     if 'name' in session:
-        if genre=="Scifi":
+        if genre == "Scifi":
             genre = "Science Fiction"
-        with open('results/genres.pkl','rb') as f:
+        with open('results/genres.pkl', 'rb') as f:
             dict = pkl.load(f)
-        return render_template("genre.html", list = dict[genre], name =session['name'])
+        return render_template("genre.html", list=dict[genre], name=session['name'])
     return redirect('/')
+
 
 @app.route('/top_rated')
 def top():
     if 'name' in session:
-        with open('results/top_rated.pkl','rb') as f:
+        with open('results/top_rated.pkl', 'rb') as f:
             data = pkl.load(f)
-        return render_template('genre.html', list = data, name =session['name'])
+        return render_template('genre.html', list=data, name=session['name'])
     return redirect('/')
+
 
 app.run(debug=True)
